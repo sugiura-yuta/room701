8ac7995b8296f9f61fb2f964d91a4dd188d7a5bd
Yuta Sugiura <y.sugiura@poksha.com>
2026-03-25 16:54:15 +0900

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Yuta Sugiura | Security x Legal Mind";
+export const SITE_DESCRIPTION =
+  "情報処理安全確保支援士・AWS Security Specialty 保有のフルスタックエンジニア。リーガルマインドを活かした設計と実装を発信。";
